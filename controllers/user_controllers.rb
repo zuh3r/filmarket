187c7fe646654ebc7e575805a26355d21fcea9d7
zvher <zuher.feki17@gmail.com>
2022-02-23 20:21:50 +1100

    update

--- controllers/user_controllers.rb
+++ controllers/user_controllers.rb
@@ -9,7 +9,7 @@ get '/sign_up' do
     email = params['email']
     password = params['password']
   
-    create_user(first_name, last_name, email, password)
+    create_user(first_name, last_name, email, password, balance)
   
     redirect '/marketplace'
   end
