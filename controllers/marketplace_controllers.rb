8970ad06a4a772c341a04247be83e154b57dd15c
zvher <zuher.feki17@gmail.com>
2022-02-23 17:07:57 +1100

    update

--- controllers/marketplace_controllers.rb
+++ controllers/marketplace_controllers.rb
@@ -1,4 +1,9 @@
 get '/' do
-    erb :'marketplace/index'
+
+    sale_images = all_sale_images()
+
+    erb :'marketplace/index', locals: {
+      sale_images: sale_images
+    }
   end
   
--- db/schema.sql
+++ db/schema.sql
@@ -22,3 +22,15 @@ CREATE TABLE gallery (
 
 
 
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('New York', 'https://images.unsplash.com/photo-1514565131-fce0801e5785?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2t5bGluZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'New York City skyline', 250);
+
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('Chicago', 'https://images.unsplash.com/photo-1534298261662-f8fdd25317db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2t5bGluZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'Chicago skyline', 180);
+
+
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('Miami', 'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2t5bGluZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'Miami skyline', 210);
+
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('Tokyo', 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG9reW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'Tokyo shopping mall', 200);
+
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('Tokyo-Street', 'https://images.unsplash.com/photo-1536768139911-e290a59011e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dG9reW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'Tokyo at night', 220);
+
+INSERT INTO marketplace(name, image_url, artist, caption, price) VALUES('Paris', 'https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60', 'Zuher Feki', 'Bridge over water in Paris', 230);
