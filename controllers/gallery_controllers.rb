d689c5cdaaad835ce306b237df194c47652aaba2
zvher <zuher.feki17@gmail.com>
2022-02-23 19:36:42 +1100

    update

--- controllers/gallery_controllers.rb
+++ controllers/gallery_controllers.rb
@@ -1,24 +1,30 @@
 get '/gallery' do
-    erb :
+    id = params["id"]
+
+    gallery_images = get_gallery_images(id)
+    erb :'gallery/gallery', locals: {
+        gallery_images: gallery_images
+        id: id
+    }
 end
 
 
 get '/gallery/new' do
     erb :'gallery/new'
-  end
+end
 
-  post '/new_art' do
+post '/new_art' do
     name = params["name"]
     image_url = params["image_url"]
     artist = params["artist"]
     caption = params["caption"]
     price = params["price"]
-    
+
     create_gallery_images(name, image_url, artist, caption, price)
     redirect '/gallery'
-  end
+end
 
-  put 'gallery/:id' do
+put 'gallery/:id' do
     id = params["id"]
     name = params["name"]
     caption = params["caption"]
@@ -26,5 +32,4 @@ get '/gallery/new' do
 
     update_gallery_images(id, name, caption, price)
     redirect '/gallery'
-  end
-  
+end
