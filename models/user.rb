05a4050a6c9cc6eadfce75f6f7da28e7adb0600e
zvher <zuher.feki17@gmail.com>
2022-02-23 20:07:00 +1100

    update

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,4 @@
-def create_user(first_name, last_name, email, password, balance)
+def create_user(first_name, last_name, email, password)
     password_digest = BCrypt::Password.create(password)
 
     run_sql("INSERT INTO users(first_name, last_name, email, password_digest) VALUES ($1, $2, $3, $4, 0)", [first_name, last_name, email, password_digest])
